c1b8d947d29d23aa3299d554d5a4a13dce504a23
cbowu <cuibowu@gmail.com>
2017-09-07 20:55:04 +0800

    modify

--- src/Solution122.java
+++ src/Solution122.java
@@ -3,11 +3,11 @@
  */
 public class Solution122 {
     public int maxProfit(int[] prices) {
-        int res = 0 ;
-        for(int i=0;i<prices.length-1;i++){
-            if(prices[i+1]>prices[i])
-                res+=prices[i+1]-prices[i];
-            return res;
+        int res = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i])
+                res += prices[i + 1] - prices[i];
         }
+        return res;
     }
 }
